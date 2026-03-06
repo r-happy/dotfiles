9b3dc25a88baaca2dbe96c68c23859e4a086a5af
r-happy <106812882+r-happy@users.noreply.github.com>
2026-03-06 16:34:25 +0900

    fix

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -66,7 +66,7 @@ vim.opt.swapfile = false
 vim.opt.showcmd = true
 
 -- color
-vim.cmd("colorscheme kangawa-dragon")
+vim.cmd("colorscheme kanagawa-dragon")
 
 -- plugins
 local disabled_builtins = {
